154f0572f3a115ca276dc42ca063d1b23eded9fb
DiegoManriquezDev <diegodev.manriquez@gmail.com>
2022-08-03 18:24:49 -0400

    Update

--- Clases y Objetos/main.py
+++ Clases y Objetos/main.py
@@ -1,15 +1,21 @@
 class Vehiculo:
-    color = "Negro"
-    ruedas = 4
-    puertas = 5
+    def __init__(self,color,ruedas,puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+
+    def __str__(self):
+        return 'El color del auto es {}, tiene {} ruedas y {} puertas'.format(self.color,self.ruedas,self.puertas)
+
 
 class Coche(Vehiculo):
-    velocidad = 120.0
-    cilindrada = 3000
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        super().__init__(color,ruedas,puertas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
 
-coche = Coche()
-print(coche.color)
-print(coche.ruedas)
-print(coche.puertas)
-print(coche.velocidad)
-print(coche.cilindrada)
+    def __str__(self):
+        return 'El color del auto es {}, tiene {} ruedas y {} puertas. Su velocidad es de {} y cuenta con {} cilindradas'.format(self.color,self.ruedas,self.puertas,self.velocidad,self.cilindrada)
+
+coche = Coche('Negro',4,5,120,3500)
+print(coche)
