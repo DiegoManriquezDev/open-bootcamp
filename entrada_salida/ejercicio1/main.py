515df9e11c1f7fcb6ded894d0104683bb39aa7bf
DiegoManriquezDev <diegodev.manriquez@gmail.com>
2022-08-08 19:42:49 -0400

    updating

--- entrada_salida/ejercicio1/main.py
+++ entrada_salida/ejercicio1/main.py
@@ -1,7 +1,14 @@
-f = open('prueba.txt','a')
-
+f = open('prueba.txt','w')
 texto = input('Agrega un texto al fichero: ')
 texto += '\n'
-
 f.write(texto)
+f.close()
+
+f = open('prueba.txt','r+')
+texto2 = input('Agrega un texto al fichero: ')
+texto2 += '\n'
+f.readline()
+f.write(texto2)
+
+print(f.read())
 f.close()
